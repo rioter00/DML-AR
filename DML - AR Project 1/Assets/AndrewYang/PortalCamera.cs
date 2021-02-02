467ea5141effdc38fa0adbaf019996bcf03075fd
Yangac14 <50116397+Yangac14@users.noreply.github.com>
2021-02-02 14:13:39 -0600

    Updated Portal Camera Script
    This single line of code is what I found to finally be the thing to help rotate the other camera to show the correct texture of the portal

--- DML - AR Project 1/Assets/AndrewYang/PortalCamera.cs
+++ DML - AR Project 1/Assets/AndrewYang/PortalCamera.cs
@@ -10,9 +10,10 @@ public class PortalCamera : MonoBehaviour   {
 
     // Update is called once per frame
     void Update(){
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y * -1f, transform.rotation.z);
         Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
         transform.position = portal.position + playerOffsetFromPortal;
-
+        
         float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
         Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
         Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
